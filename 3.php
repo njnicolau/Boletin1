569b2d173e90ab4dd5303d80ba0debe17573e150
nicolau <nicolau@Ares>
2017-03-21 20:09:38 +0100

    exercici 3 finalitzat

--- 3.php
+++ 3.php
@@ -1,6 +1,6 @@
 <?php
 
-/* exercici no finalitzat
+/* exercici finalitzat
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
